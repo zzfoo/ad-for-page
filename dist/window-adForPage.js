f463685b7d95b1eb2dbe06f9f3abbbdaadd6f0b3
zzfoo <842176206@qq.com>
2018-12-24 13:25:19 +0800

    build

--- dist/window-adForPage.js
+++ dist/window-adForPage.js
@@ -435,9 +435,9 @@ var proto = {
     doShowAd: function (name, callback) {
         var ad = this._adCache[name];
         ad.show().then(function() {
-            callback(null);
+            callback && callback(null);
         }).catch(function(err) {
-            callback(err);
+            callback && callback(err);
         })
     },
 
